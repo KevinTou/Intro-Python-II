9abfa258731ae6c58a04cb0f728cf04a33795694
Kevin Tou <toukevin@gmail.com>
2019-12-04 14:13:22 -0800

    Created a Room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,22 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# Attributes
+# name
+# description
+
+# n_to, s_to, e_to, and w_to
+# which point to the room in that respective direction.
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return f"\nname: {self.name}\ndescription: {self.description}\n"
